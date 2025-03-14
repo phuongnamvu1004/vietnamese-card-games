97c353c3eacc5942e214a55dbbd08b4443f2b44a
vphuongnam <nvu@oberlin.edu>
2025-03-14 16:10:21 -0400

    initialize repo

---
+++ src/client/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Vietnamese Card Games</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
